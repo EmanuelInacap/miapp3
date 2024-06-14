c4821459713b3e33f2fa502ba9269aae0b254083
EmanuelInacap <emanuel.sanchez03@inacapmail.cl>
2024-06-14 16:23:59 -0400

    Updated

---
+++ src/Datos.jsx
@@ -0,0 +1,18 @@
+function Datos(props){
+    return(
+        <div>
+            {
+                props.alumnos.map((datosAlumnos)=>(
+                    <div>
+                        <h1>{datosAlumnos.nombre}</h1>
+                        <h1>{datosAlumnos.edad}</h1>
+                    </div>
+                ))
+            }
+
+
+        </div>
+    )
+}
+
+export default Datos;
